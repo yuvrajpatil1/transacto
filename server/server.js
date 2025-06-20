47cac910ce69a7fcb2c77027656c062ddd6ebe20
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-06-20 23:31:03 +0530

    ..

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: "https://transacto01.netlify.app/",
+    origin: "https://transacto01.onrender.com/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
