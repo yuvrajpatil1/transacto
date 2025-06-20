7e26ad475c4adbfed5b0633ee4775968316ac06d
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-06-20 14:39:56 +0530

    render-backend-url-updated

--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://transacto-backend.onrender.com",
   withCredentials: true,
 });
 
