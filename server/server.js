19003052bd4ed3606ca9f87d7a9b3058efae675b
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-06-20 22:58:54 +0530

    package.json-added-homepage-url

--- server/server.js
+++ server/server.js
@@ -15,13 +15,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://transacto.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
