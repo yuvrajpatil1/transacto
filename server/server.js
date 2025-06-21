17a4cc741edd585e76944679a3ed565325679652
Yuvraj Patil <yuvrajpatil12092004@gmail.com>
2025-06-21 22:07:50 +0530

    ..

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: ["https://transacto.onrender.com", "http://localhost:5173"],
+    origin: "https://transacto.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
